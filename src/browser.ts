d615e0cfff012723149c471bd97396096642b156
xiongxiao <kevisual@xiongxiao.me>
2026-04-24 16:47:37 +0800

    update: bump version to 0.2.11 and export listen-process

--- src/browser.ts
+++ src/browser.ts
@@ -13,4 +13,6 @@ export { createSkill, tool, fromJSONSchema, toJSONSchema } from './route.ts';
 export { CustomError } from './result/error.ts';
 
 export * from './router-define.ts';
+
+export * from './utils/listen-process.ts'
 // --- 以上同步更新至 browser.ts ---
